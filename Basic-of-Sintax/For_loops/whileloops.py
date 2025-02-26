d64db0013e6968d750402c6c98ce949f0adef9b0
BlackWaterPark0011010111 <blairdeleon@protonmail.com>
2025-02-26 23:07:23 +0100

    b

---
+++ Basic-of-Sintax/For_loops/whileloops.py
@@ -0,0 +1,67 @@
+
+#temp = 4
+#while temp<21:
+ #   temp+=1
+ #   print(f'heating on temp is {temp}')
+
+print('+++++++++++++++++++++++++++++++++++++++++++++')
+
+temp = input(( 'ure temp is: '))
+if int(temp)<25:
+     print(f'ure temp is {temp}, its okay')
+else:
+    print('turn ure air condition on')
+    
+temp = 16
+while temp<20:
+     temp+=1
+     print(f'turn n hitting, {temp}')
+
+while temp>25:
+    temp-=1
+    print(f'turn it on, cuz temp is {temp}')      
+print('+++++++++++++++++++++++++++++++++++++++++++++')
+
+a = input('ure first number:  ')
+b = input('ure second number:  ')
+
+a = input('U`re number is:  ')
+while temp<10:
+     temp = a*+1
+print(f'Ure result is:   {temp}̈́')
+print('+++++++++++++++++++++++++++++++++++++++++++++')
+
+     
+number = int(input("Enter a number: "))
+for i in range(1, 11):
+        result = number * i
+        print(f"{number} x {i} = {result}")
+
+x = input('WTF do u want to reverse: ')
+        
+print(*reversed(x))
+
+  
+x = 27
+print(bin(x))
+
+x = 30
+h = hex(x)
+print(h)
+
+i = 0b11011
+
+print (hex(i)) 
+
+
+
+
+
+n = int(input('Please enter any integer: '))
+for i in range(n):
+    for j in range(i,i+1):
+        print(j, end= '  ')
+    print()
+
+
+

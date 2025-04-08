6308459939134351f1f0ad1fc825d4a7b63d6b28
BlackWaterPark0011010111 <blairdeleon@protonmail.com>
2025-04-08 22:46:34 +0200

    fixing  pasword.py

--- Working with Files and Streams/pasword.py
+++ Working with Files and Streams/pasword.py
@@ -29,25 +29,6 @@ def main():
     except Exception as e:
         print(f"Произошла ошибка: {e}")
     
-    print("\n=== Тест 4: Граничные случаи ===")
-    
-    empty_input = getpass.getpass("Попробуйте ввести пустой пароль (просто Enter): ")
-   #empty_input = getpass.getpass(prompt="", stream=None)  #альтернатива
-    if not empty_input:
-        print("Вы ввели пустой пароль!")
-    else:
-        print("Вы ввели непустой пароль длиной", len(empty_input))
-    
-    print("\n=== Тест 5: Ввод непарольных данных ===")
-    
-    number_input = getpass.getpass("Введите число скрыто: ")
-    try:
-        number = int(number_input)
-        print(f"Вы ввели число: {number}")
-       #print("Квадрат числа:", number**2)  
-    except ValueError:
-        print("Это не число!")
-    
     print("\n=== Выводы ===")
     print("1. getpass надежно скрывает ввод в консоли")
     print("2. В IDE может работать некорректно")
